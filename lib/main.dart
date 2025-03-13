16d11fc24a079f7b6da1bcea82fa6b1614d7cde5
rutu bhandari <rutubhandari@gmail.com>
2025-03-13 10:52:34 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+<<<<<<< HEAD
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'screens/login_screen.dart';
@@ -9,6 +10,11 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(); // Initialize Firebase
   runApp(MyApp());
+=======
+
+void main() {
+  runApp(const MyApp());
+>>>>>>> e6bc8161ff7dd266d24985a1923abd7ca2753b08
 }
 
 class MyApp extends StatelessWidget {
@@ -17,6 +23,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+<<<<<<< HEAD
       debugShowCheckedModeBanner: false,
       title: "AI Lecture Notes",
       theme: ThemeData(primarySwatch: Colors.indigo),
@@ -26,10 +33,19 @@ class MyApp extends StatelessWidget {
         "/signup": (context) => SignupScreen(),
         "/home": (context) => BottomNavBar(), // Navigate to BottomNavBar
       },
+=======
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+>>>>>>> e6bc8161ff7dd266d24985a1923abd7ca2753b08
     );
   }
 }
 
+<<<<<<< HEAD
 // ✅ Decide whether to show Login or BottomNavBar
 class AuthWrapper extends StatelessWidget {
   const AuthWrapper({super.key});
@@ -47,6 +63,54 @@ class AuthWrapper extends StatelessWidget {
         }
         return LoginScreen(); // Show Login Page
       },
+=======
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+>>>>>>> e6bc8161ff7dd266d24985a1923abd7ca2753b08
     );
   }
 }
